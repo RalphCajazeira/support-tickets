e743e289fea9207a8ff9b2570394ac4dbc9bd3ca
Ralph Ramos <ralphmtk@gmail.com>
2025-07-28 14:46:02 -0300

    Criando Banco de Dados

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,10 +1,10 @@
+import { create } from "../controllers/tickets/create.js";
+
 export const tickets = [
   {
     method: "POST",
     path: "/tickets",
-    controller: (request, response) => {
-      response.end("Criado com sucesso!");
-    },
+    controller: create,
   },
   {
     method: "GET",
